2eb1b95743307579838ee2c13ab85b61b0b691f6
Ishii-Nozomi <xx2468.egg@gmail.com>
2023-01-21 22:54:02 +0900

    ④を追加、③を修正

--- Contents.swift
+++ Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 import PlaygroundSupport
 
 
@@ -15,11 +16,50 @@ func microwave(openTheDoor: Bool) -> Void {
 }
 microwave(openTheDoor: true)
 
-// ②タイマーの時間を表示する
+// ②ワット数を決める
+var wattButton = ["500w", "600w", "1000w"]
+print(wattButton)
+private func wattButtonInfo(wattButton: String) {
+    switch wattButton {
+    case "500w" :
+        print("500wで温める")
+    case "600w" :
+        print("600wで温める")
+    case "1000w" :
+        print("1000wで温める")
+    default :
+        print("設定できません。")
+    }
+}
+wattButtonInfo(wattButton: "500w")
+
+// ③タイマーの時間を表示する
 var timerButton = ["1分", "50秒", "40秒", "30秒", "20秒", "10秒"]
 print(timerButton)
-// ③温める時間を設定する
 
-// ④ボタンを押して温めスタート
+// ④温める時間を設定する
+private func timerButtonInfo(timerButton: String) {
+    switch timerButton {
+    case "1分" :
+        print("1分設定")
+    case "50秒":
+        print("50秒設定")
+    case "40秒":
+        print("40秒設定")
+    case "30秒":
+        print("30秒設定")
+    case "20秒":
+        print("20秒設定")
+    case "10秒":
+        print("10秒設定")
+    default:
+        print("設定できません。")
+    }
+}
+timerButtonInfo(timerButton: "1分")
+
+// ⑤ボタンを押して温めスタート
+let startButton = "スタート"
+
 
 
